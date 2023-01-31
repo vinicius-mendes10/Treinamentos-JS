14d251b161c84260475a1e23ad174f856a5998c0
Vinicius Ferreira Mendes <viny@Viniciuss-MacBook-Pro.local>
2023-01-31 14:36:02 -0300

    feat: treinando condicional, if e else, verificaçøes verdadeiro e falso

---
+++ script.js
@@ -0,0 +1,15 @@
+// let ok = false;
+
+// if(ok) {
+//     console.log('entrou no if');
+// }else {
+//     console.log('nåo entrou no if');
+// }
+let idade = 36;
+let adulto = idade >= 18 && idade < 60;
+
+if(adulto){
+    console.log('E adulto');
+}else{
+    console.log('Nåo e adulto');
+}
