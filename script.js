1f48b3a3bffbc2f6174d76cc3d8dac26f64f870b
Vinicius Ferreira Mendes <viny@Viniciuss-MacBook-Pro.local>
2023-01-31 14:57:51 -0300

    feat: realizado desafios de condicionais simples, if e else

--- script.js
+++ script.js
@@ -5,11 +5,32 @@
 // }else {
 //     console.log('nåo entrou no if');
 // }
-let idade = 36;
-let adulto = idade >= 18 && idade < 60;
+// let idade = 36;
+// let adulto = idade >= 18 && idade < 60;
 
-if(adulto){
-    console.log('E adulto');
+// if(adulto){
+//     console.log('E adulto');
+// }else{
+//     console.log('Nåo e adulto');
+// }
+
+//exercicio if e else
+
+let x = 10;
+let y = 5;
+console.log(x > y);
+
+let w = "10";
+let z = 10;
+console.log( w === z);
+
+let a = 10;
+let b = 5;
+console.log( a > b);
+
+let preco = 40.3;
+if (preco <= 45){
+    console.log('A carne esta barata');
 }else{
-    console.log('Nåo e adulto');
+    console.log('Acarne esta cara');
 }
